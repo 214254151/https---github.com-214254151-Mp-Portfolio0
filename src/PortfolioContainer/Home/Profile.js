c720b0a4f873a418bcdd801bcbf8c8486bd7d6e7
Abongile Tshopi <abongiletshopi@gmail.com>
2024-06-12 15:37:07 +0200

    implimented react-typical feature

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Typical from "react-typical";
+import Typical from 'react-typical';
 
 
 
@@ -13,18 +15,49 @@ export default function Profile() {
           <div className="colz ">
 
             <a href="https://www.youtube.com/watch?v=ilw_g_rxtVc">
-              <i className="bi bi-facebook"></i>
+            <i class="bi bi-youtube"></i>
             </a>
 
-            <a href="#">
+            <a href="https://www.linkedin.com/in/abongile-tshopi-05b594169/#">
             <i class="bi bi-linkedin"></i>
             </a>
 
-            <a href="#">
+            <a href="https://github.com/214254151">
               <i class="bi bi-github"></i>
             </a>
         
           </div>
+
+          <div className="profile-details-name">
+            <span className="primary-text">
+                {" "}
+                Hello, I'M <span className="highlighted-text">Abongile</span>
+            </span>
+          </div>
+
+          <div className="profile-details-role">
+            <span className="primary-text">
+                {" "}
+                <h1>
+                <Typical
+                loop={Infinity}
+                steps={[
+                    "Enthusiastic Dev", 
+                    1000, 
+                    "Full Stack Developer ", 
+                    1000, 
+                    "Cross Platform Dev", 
+                    1000, 
+                    "React/Angular Dev", 
+                    1000, 
+                    "Java Developer", 
+                    1000, 
+                ]}
+                />
+                </h1>
+            </span>
+          </div>
+
         </div>
       </div>
     </div>
