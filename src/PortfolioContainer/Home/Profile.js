50e7ac0a38c2f44357379d87f78f28e38eeff7c0
Abongile Tshopi <abongiletshopi@gmail.com>
2024-06-12 14:41:36 +0200

    icons fixed import and istalation the package

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+
+
+
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz ">
+
+            <a href="#">
+              <i className="bi bi-facebook"></i>
+            </a>
+
+            <a href="#">
+            <i class="bi bi-linkedin"></i>
+            </a>
+
+            <a href="#">
+              <i class="bi bi-github"></i>
+            </a>
+        
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
