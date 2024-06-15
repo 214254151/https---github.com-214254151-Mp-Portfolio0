c27f5cc4b86df06c6d657e0beb809b77c0b54524
Abongile Tshopi <abongiletshopi@gmail.com>
2024-06-15 16:54:38 +0200

    updates

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+
+
+
+
+function Profile(props) {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className ='profile-details'>
+                    <div className='colz'>
+                    
+                    < a href="#">
+                        <i className='fa fa-facebook-square'></i>
+                        </a>
+                        <i class="bi bi-linkedin"></i>
+                        <i class="bi bi-wifi"></i>
+
+                        < a href="#">
+                        <i className='fa fa-github-square'></i>
+                        </a>
+
+                                hi
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
