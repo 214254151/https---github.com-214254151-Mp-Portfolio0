3cb7c5fd4472a2fbffa0b2944b5a767752bbc142
Abongile Tshopi <abongiletshopi@gmail.com>
2024-06-18 00:02:51 +0200

    ...

---
+++ src/PortfolioContainer/Home/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react" 
+import Profiler  from "./Profile"
+import Footer from "./Footer/Footer.js"
+
+ export default function Footer(){
+    return(
+        <div className="home-container">
+
+
+        </div>
+    )
+
+ }
