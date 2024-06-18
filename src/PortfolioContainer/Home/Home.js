45ee478157fbcff41a8b8abfffe3965eccd61e1b
Abongile Tshopi <abongiletshopi@gmail.com>
2024-06-18 02:21:24 +0200

    rendering home.js

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Profile from "./Profile/Profile";
+import Footer from "./Footer/Footer";
+import "./Home.css";
+
 
 export default function Home(){
     return(
-        <div>
-            
+        <div className="home-container">
+            <Profile/>
+            <Footer/>
         </div>
     )
 }
