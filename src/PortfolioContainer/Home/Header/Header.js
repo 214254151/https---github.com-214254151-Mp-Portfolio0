18fb04a31d248a603faf18c65d009028e9f886e5
Abongile Tshopi <abongiletshopi@gmail.com>
2024-06-19 15:32:25 +0200

    Making header, UI for home

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -13,15 +13,60 @@ export default function Header(){
         if (!currentScreen || !currentScreen.screenInView)
             return;
         let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
-        if(screen < 0)
+        if(screenIndex < 0)
             return
     }
 
     let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen)
+
+    const getHeaderOptions = ()=>{
+        return(
+            TOTAL_SCREENS.map((screen, i)=>(
+                <div key={screen.screen_name} className={getHeaderOptionsClass(i)}
+                onClick={() => switchScreen(i)}>
+                <span>{screen.screen_name}</span>
+                </div>
+            ))
+        )    
+    }
+
+    const getHeaderOptionsClass = (index)=>{
+        let classes = "header-option ";
+        if(index < TOTAL_SCREENS.length -1)
+            classes += "header-option-seperator";
+        if(selectedScreen === index)
+            classes += "selected-header-option";
+        return
+    }
+
+    const switchScreen = (index, screen)=>{
+        let screenComponent = document.getElementById(screen.screen_name);
+        if(!screenComponent)
+            return;
+
+        screenComponent.scrollIntoView({behavior: "smooth"});
+        setSelectedScreen(index);
+        setShowHeadersOptions(false);
+    }
+    
     
     return(
-        <div>
-
-        </div>
+        
+            <div className="header-container" onClick={()=> (!setShowHeadersOptions)}>
+                <div className="header-parent">
+                    <div className="header-hamburger" 
+                    onClick={()=>setShowHeadersOptions(!showHeaderOptions)}>
+                        <FontAwesomeIcon className="header-hamburger-bars" icon={faBars}/>
+                    </div>
+                    <div className="header-logo">
+                        <span>Abongile</span> 
+                    </div>            {/* <img src={logo} alt="logo"/> */}
+                        <div className={showHeaderOptions 
+                            ? "header-options showhambuger-options"
+                            : "header-options"}>
+                            {getHeaderOptions()}
+                        </div>
+                </div>
+            </div>
     )
 }
