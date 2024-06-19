21bfa49d883389879da0e686d5d88e51a77b3d73
Abongile Tshopi <abongiletshopi@gmail.com>
2024-06-19 19:26:05 +0200

    code strcture

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -1,72 +1,80 @@
 import React, { useState } from "react";
-import { TOTAL_SCREENS, GET_SCREEN_INDEX } from "../../../utilities/commonUtils";
+import {
+  TOTAL_SCREENS,
+  GET_SCREEN_INDEX,
+} from "../../../utilities/commonUtils";
 import ScrollService from "../../../utilities/ScollService";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Header.css";
 
-export default function Header(){
-    const [selectedScreen, setSelectedScreen] = useState(0)
-    const [showHeaderOptions, setShowHeadersOptions] = useState(false)
+export default function Header() {
+  const [selectedScreen, setSelectedScreen] = useState(0);
+  const [showHeaderOptions, setShowHeadersOptions] = useState(false);
 
-    const updateCurrentScreen = (currentScreen) =>{
-        if (!currentScreen || !currentScreen.screenInView)
-            return;
-        let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
-        if(screenIndex < 0)
-            return
-    }
+  const updateCurrentScreen = (currentScreen) => {
+    if (!currentScreen || !currentScreen.screenInView) return;
+    let screenIndex = GET_SCREEN_INDEX(currentScreen.screenInView);
+    if (screenIndex < 0) return;
+  };
 
-    let currentScreenSubscription = ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen)
+  let currentScreenSubscription =
+    ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
 
-    const getHeaderOptions = ()=>{
-        return(
-            TOTAL_SCREENS.map((screen, i)=>(
-                <div key={screen.screen_name} className={getHeaderOptionsClass(i)}
-                onClick={() => switchScreen(i)}>
-                <span>{screen.screen_name}</span>
-                </div>
-            ))
-        )    
-    }
+  const getHeaderOptions = () => {
+    return TOTAL_SCREENS.map((screen, i) => (
+      <div
+        key={screen.screen_name}
+        className={getHeaderOptionsClass(i)}
+        onClick={() => switchScreen(i)}
+      >
+        <span>{screen.screen_name}</span>
+      </div>
+    ));
+  };
 
-    const getHeaderOptionsClass = (index)=>{
-        let classes = "header-option ";
-        if(index < TOTAL_SCREENS.length -1)
-            classes += "header-option-seperator";
-        if(selectedScreen === index)
-            classes += "selected-header-option";
-        return
-    }
+  const getHeaderOptionsClass = (index) => {
+    let classes = "header-option ";
+    if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator";
+    if (selectedScreen === index) classes += "selected-header-option";
+    return;
+  };
 
-    const switchScreen = (index, screen)=>{
-        let screenComponent = document.getElementById(screen.screen_name);
-        if(!screenComponent)
-            return;
+  const switchScreen = (index, screen) => {
+    let screenComponent = document.getElementById(screen.screen_name);
+    if (!screenComponent) return;
 
-        screenComponent.scrollIntoView({behavior: "smooth"});
-        setSelectedScreen(index);
-        setShowHeadersOptions(false);
-    }
-    
-    
-    return(
-        
-            <div className="header-container" onClick={()=> (!setShowHeadersOptions)}>
-                <div className="header-parent">
-                    <div className="header-hamburger" 
-                    onClick={()=>setShowHeadersOptions(!showHeaderOptions)}>
-                        <FontAwesomeIcon className="header-hamburger-bars" icon={faBars}/>
-                    </div>
-                    <div className="header-logo">
-                        <span>Abongile</span> 
-                    </div>            {/* <img src={logo} alt="logo"/> */}
-                        <div className={showHeaderOptions 
-                            ? "header-options showhambuger-options"
-                            : "header-options"}>
-                            {getHeaderOptions()}
-                        </div>
-                </div>
-            </div>
-    )
+    screenComponent.scrollIntoView({ behavior: "smooth" });
+    setSelectedScreen(index);
+    setShowHeadersOptions(false);
+  };
+
+  return (
+    <div
+      className="header-container"
+      onClick={() => setShowHeadersOptions(!showHeaderOptions)}
+    >
+      <div className="header-parent">
+        <div
+          className="header-hamburger"
+          onClick={() => setShowHeadersOptions(!showHeaderOptions)}
+        >
+          <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+        </div>
+        <div className="header-logo">
+          <span>Abongile</span>
+        </div>
+        {/* <img src={logo} alt="logo"/> */}
+        <div
+          className={(
+            showHeaderOptions)
+              ? "header-options show-hambuger-options"
+              : "header-options"
+          }
+        >
+          {getHeaderOptions()}
+        </div>
+      </div>
+    </div>
+  );
 }
