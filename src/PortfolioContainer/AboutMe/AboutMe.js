b26f8ad63e3b2346d0da8733a91d3724d990db7d
Abongile Tshopi <abongiletshopi@gmail.com>
2024-06-23 14:06:54 +0200

    About me creation and

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,26 +1,71 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
-import ScrollService from "../../utilities/ScollService"; 
+import ScrollService from "../../utilities/ScollService";
 import Animations from "../../utilities/Animations";
 import { Highlights } from "react-bootstrap-icons";
 
-export default function AboutMe(props){
-    let fadeInScreenHandler = (screen)=>{
-        if(screen.fadeInScreen !== props.id)
-            return;
-            Animations.animations.fadeInScreen(props.id)
-    }
+export default function AboutMe(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
 
-    const fadeInSubscription = 
+  const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-    
-    
-    return(
-        <div className="about-me-conatner screen-container" id={props.id || ""}>
-            <div className="about-me-parent">
-                <ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'}/>
+  const SCREEN_CONSTANTS = {
+    description: "blabablabablablalbalblablablablablalb.",
+    Highlights: {
+      bullets: [
+        "Full Stack Developer",
+        "Frontend",
+        "Designer",
+        "Database management",
+      ],
+      Heading: "Here are a few highlights: ",
+    },
+  };
+
+  const renderHighlight = () => {
+    return SCREEN_CONSTANTS.Highlights.bullets.map((value, i) => (
+      <div className="highlight" key={i}>
+        <div className="highlight-blob"></div>
+        <span>{value}</span>
+      </div>
+    ));
+  };
+
+  return (
+    <div className="about-me-conatner screen-container" id={props.id || ""}>
+      <div className="about-me-parent">
+        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+        <div className="about-me-card">
+          <div className="about-me-profile"></div>
+          <div className="about-me-details">
+            <span className="about-me-description">
+              {SCREEN_CONSTANTS.description}
+            </span>
+            <div className="about-highlights">
+              <div className="highlight-heading">
+                <span>{SCREEN_CONSTANTS.Highlights.Heading}</span>
+              </div>
+              {renderHighlight()}
             </div>
+            <div className="about-me-options">
+              <button
+                className="btn primary-btn"
+                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+              >
+                {" "}
+                Hire Me{" "}
+              </button>
+              <a href="ABONGILE-TSHOPI-CV.pdff" download="Abongile Tshopi.pdf">
+                <button className="btn highlighted-btn">Get Resume</button>
+              </a>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
