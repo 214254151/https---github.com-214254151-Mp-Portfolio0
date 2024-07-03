18f70a18fbb662626a3679b4937ba5dda3b4dfba
Abongile Tshopi <abongiletshopi@gmail.com>
2024-07-03 15:32:03 +0200

    imported pictures into the public dir for projects

--- src/PortfolioContainer/Projects/Projects.js
+++ src/PortfolioContainer/Projects/Projects.js
@@ -1,30 +1,166 @@
 import React from "react";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading"
-import ScrollService from "../../utilities/ScrollService"
-import Animations from "../../utilities/Animations"
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
 
-export default function Projects(props){
+export default function Projects(props) {
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
 
-    let fadeInScreenHandler = (screen) => {
-        if (screen.fadeInScreen !== props.id) return;
-        Animations.animations.fadeInScreen(props.id);
-      };
-    
-      const fadeInSubscription =
-        ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-    return(
-        <div>
-            <ScreenHeading
-            title={"Projects"}
-            subHeading={"Find out abour my "}
-            />
+  return (
+    <div>
+      <ScreenHeading title={"Projects"} subHeading={"Find out abour my "} />
 
-            <section className="project-section" id={props.id || ""}>
+      <section className="project-section" id={props.id || ""}>
+        <div className="container">
+          <div className="row">
+            <owlCarousel className="owl-carousel" id="projects-carousel">
+              <div className="col-lg-12">
+                <div className="projects-item">
+                  <div className="project-description">
+                    <p>
+                      <i className="fa fa-quote-left" /> asdfasdfaghspify
+                      apiyupwqyaepoqwyetgqw qwp otquowty;oqwy ;wet{" "}
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="start list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
 
-            </section>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="project-infor">
+                    <img
+                      src="img/projects/Figma.png"
+                      alt="no internet connection"
+                    ></img>
+                    <h5>Figma design</h5>
+                    <p>Design prototype for a website</p>
+                  </div>
+                </div>
+              </div>
+
+
+
+              <div className="col-lg-12">
+                <div className="projects-item">
+                  <div className="project-description">
+                    <p>
+                      <i className="fa fa-quote-left" /> asdfasdfaghspify
+                      apiyupwqyaepoqwyetgqw qwp otquowty;oqwy ;wet{" "}
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="start list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="project-infor">
+                    <img
+                      src="img/projects/Figma.png"
+                      alt="no internet connection"
+                    ></img>
+                    <h5>Figma design</h5>
+                    <p>Design prototype for a website</p>
+                  </div>
+                </div>
+              </div>
+
+
+
+              <div className="col-lg-12">
+                <div className="projects-item">
+                  <div className="project-description">
+                    <p>
+                      <i className="fa fa-quote-left" /> asdfasdfaghspify
+                      apiyupwqyaepoqwyetgqw qwp otquowty;oqwy ;wet{" "}
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="start list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="project-infor">
+                    <img
+                      src="img/projects/Figma.png"
+                      alt="no internet connection"
+                    ></img>
+                    <h5>Figma design</h5>
+                    <p>Design prototype for a website</p>
+                  </div>
+                </div>
+              </div><div className="col-lg-12">
+                <div className="projects-item">
+                  <div className="project-description">
+                    <p>
+                      <i className="fa fa-quote-left" /> asdfasdfaghspify
+                      apiyupwqyaepoqwyetgqw qwp otquowty;oqwy ;wet{" "}
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="start list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="project-infor">
+                    <img
+                      src="img/projects/Figma.png"
+                      alt="no internet connection"
+                    ></img>
+                    <h5>Figma design</h5>
+                    <p>Design prototype for a website</p>
+                  </div>
+                </div>
+              </div>
+            </owlCarousel>
+          </div>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
+
+
