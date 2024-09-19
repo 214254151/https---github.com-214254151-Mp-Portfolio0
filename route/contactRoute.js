116fa299d26ff624941839a9093355fe6e9b1254
Abongile Tshopi <abongiletshopi@gmail.com>
2024-09-19 04:17:52 +0200

    added transport object and mailing object and

--- route/contactRoute.js
+++ route/contactRoute.js
@@ -1,4 +1,40 @@
 const router = require('express').Router();
 const nodemailer = require('nodemailer');
 
-router.post('/contact'); 
+router.post('/contact', (req, res)=>{
+    let data = req.body
+    if(data.name.length === 0 || data.email.length === 0 || data.message.length === 0){
+        return res.json({msg: "please fill athe the fields"});
+
+        // transpot object
+        let smtpTransport = nodemailer.createTransport({
+            service: 'Gmail',
+            port:465,
+            aut:{
+                user: 'abongiletshopi@gmail.com',
+                pass: '512256TsG#@#'
+            }
+        })
+
+        // mailing object
+        let mailOptions ={
+            from:data.email,
+            to:'abongiletshopi@gmail.com',
+            subject: 'message from $(data.name)',
+            
+            html:`
+            
+            <h3>Information<h3/>
+            <ul>
+                <li>Name: $(data.name)<li/>
+                <li>Email: $(data.email)<li/>
+            <ul/>
+
+            <h3>Message<h3/>
+            <p>$(data.message)<p/>
+            `
+
+
+        }
+    }
+}) 
